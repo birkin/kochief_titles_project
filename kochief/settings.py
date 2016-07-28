32edc4e9f196f4dc70390e9900d388f219898c83
birkin <birkin.diana@gmail.com>
2016-07-28 08:55:32 -0400

    updates settings.

--- kochief/settings.py
+++ kochief/settings.py
@@ -74,12 +74,14 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = BASE_DIR + 'media/'
+# MEDIA_ROOT = BASE_DIR + 'media/'
+MEDIA_ROOT = unicode( os.environ['KC_NWTTLS__MEDIA_ROOT'] )
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = BASE_URL + 'media/'
+# MEDIA_URL = BASE_URL + 'media/'
+MEDIA_URL = unicode( os.environ['KC_NWTTLS__MEDIA_URL'] )
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
