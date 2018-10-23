0db9ec05557ed742c28c79b36a41f4468c21b532
birkin <birkin.diana@gmail.com>
2018-10-23 16:24:53 -0400

    temporarily disables index-removal.

--- kochief/cron_scripts/nightly_update.sh
+++ kochief/cron_scripts/nightly_update.sh
@@ -6,8 +6,14 @@
 export C_LOG_PATH=$KC_NWTTLS__CONTROLLER_LOG_PATH
 export C_KOCHIEF_DIR_PATH=$KC_NWTTLS__KOCHIEF_DIR_PATH
 export C_PYTHON_PATH=$KC_NWTTLS__PYTHON_PATH
+echo "python path is... $C_PYTHON_PATH"
+
 export C_PARSER_PATH=$KC_NWTTLS__PARSER_PATH
+echo "parser-path is... $C_PARSER_PATH"
+
 export C_UPDATES_DIR_PATH=$KC_NWTTLS__UPDATES_DIR_PATH  # include trailing slash
+echo "updates dir path is... $C_UPDATES_DIR_PATH"
+
 export C_PROJECT_URL=$KC_NWTTLS__BASE_URL  # include trailing slash
 
 echo " " >> $C_LOG_PATH
@@ -16,6 +22,8 @@ echo "$(date +%Y-%d-%b_%H-%M-%S) - starting nightly script" >> $C_LOG_PATH
 
 echo "$(date +%Y-%d-%b_%H-%M-%S) - about to parse records and update solr" >> $C_LOG_PATH
 cd $C_KOCHIEF_DIR_PATH
+echo "kochief dir path is... $C_KOCHIEF_DIR_PATH"
+
 # echo "$(date +%Y-%d-%b_%H-%M-%S) - pwd, $(pwd)" >> $C_LOG_PATH
 # $C_PYTHON_PATH $C_PARSER_PATH $C_UPDATES_DIR_PATH >> $C_LOG_PATH
 # $C_PYTHON_PATH $C_PARSER_PATH $C_UPDATES_DIR_PATH >> $C_LOG_PATH 2>&1
@@ -24,8 +32,7 @@ echo "$(date +%Y-%d-%b_%H-%M-%S) - parse and solr commit step completed" >> $C_L
 echo " " >> $C_LOG_PATH
 
 echo "$(date +%Y-%d-%b_%H-%M-%S) - about to remove 'expired' records from index" >> $C_LOG_PATH
-# $C_PYTHON_PATH manage.py index -expire >> $C_LOG_PATH 2>&1
-nice -19 $C_PYTHON_PATH manage.py index -expire >> $C_LOG_PATH 2>&1
+# nice -19 $C_PYTHON_PATH manage.py index -expire >> $C_LOG_PATH 2>&1
 echo " " >> $C_LOG_PATH
 
 echo "$(date +%Y-%d-%b_%H-%M-%S) - about to optimize the index" >> $C_LOG_PATH
