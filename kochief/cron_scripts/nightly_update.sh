32769b87ba8ff111ace10b0384039e5b39a76b1d
birkin <birkin.diana@gmail.com>
2018-10-25 13:58:38 -0400

    temporarily disables expire.

--- kochief/cron_scripts/nightly_update.sh
+++ kochief/cron_scripts/nightly_update.sh
@@ -19,20 +19,17 @@ export C_PROJECT_URL=$KC_NWTTLS__BASE_URL  # include trailing slash
 echo " " >> $C_LOG_PATH
 echo "-------" >> $C_LOG_PATH
 echo "$(date +%Y-%d-%b_%H-%M-%S) - starting nightly script" >> $C_LOG_PATH
+echo " " >> $C_LOG_PATH
 
 echo "$(date +%Y-%d-%b_%H-%M-%S) - about to parse records and update solr" >> $C_LOG_PATH
 cd $C_KOCHIEF_DIR_PATH
-echo "kochief dir path is... $C_KOCHIEF_DIR_PATH"
-
-# echo "$(date +%Y-%d-%b_%H-%M-%S) - pwd, $(pwd)" >> $C_LOG_PATH
-# $C_PYTHON_PATH $C_PARSER_PATH $C_UPDATES_DIR_PATH >> $C_LOG_PATH
-# $C_PYTHON_PATH $C_PARSER_PATH $C_UPDATES_DIR_PATH >> $C_LOG_PATH 2>&1
+# echo "kochief dir path is... $C_KOCHIEF_DIR_PATH"
 nice -19 $C_PYTHON_PATH $C_PARSER_PATH $C_UPDATES_DIR_PATH >> $C_LOG_PATH 2>&1
 echo "$(date +%Y-%d-%b_%H-%M-%S) - parse and solr commit step completed" >> $C_LOG_PATH
 echo " " >> $C_LOG_PATH
 
 echo "$(date +%Y-%d-%b_%H-%M-%S) - about to remove 'expired' records from index" >> $C_LOG_PATH
-# nice -19 $C_PYTHON_PATH manage.py index -expire >> $C_LOG_PATH 2>&1
+# nice -19 $C_PYTHON_PATH manage.py index -expired >> $C_LOG_PATH 2>&1
 echo " " >> $C_LOG_PATH
 
 echo "$(date +%Y-%d-%b_%H-%M-%S) - about to optimize the index" >> $C_LOG_PATH
