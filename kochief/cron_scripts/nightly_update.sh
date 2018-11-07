877951f7e196d458375ac6f8e58472f8d7dbb74f
birkin <birkin.diana@gmail.com>
2018-11-07 15:24:03 -0500

    directs echo output to log.

--- kochief/cron_scripts/nightly_update.sh
+++ kochief/cron_scripts/nightly_update.sh
@@ -6,13 +6,16 @@
 export C_LOG_PATH=$KC_NWTTLS__CONTROLLER_LOG_PATH
 export C_KOCHIEF_DIR_PATH=$KC_NWTTLS__KOCHIEF_DIR_PATH
 export C_PYTHON_PATH=$KC_NWTTLS__PYTHON_PATH
-echo "python path is... $C_PYTHON_PATH"
+echo "python path is... $C_PYTHON_PATH" >> $C_LOG_PATH
+echo " " >> $C_LOG_PATH
 
 export C_PARSER_PATH=$KC_NWTTLS__PARSER_PATH
-echo "parser-path is... $C_PARSER_PATH"
+echo "parser-path is... $C_PARSER_PATH" >> $C_LOG_PATH
+echo " " >> $C_LOG_PATH
 
 export C_UPDATES_DIR_PATH=$KC_NWTTLS__UPDATES_DIR_PATH  # include trailing slash
-echo "updates dir path is... $C_UPDATES_DIR_PATH"
+echo "updates dir path is... $C_UPDATES_DIR_PATH" >> $C_LOG_PATH
+echo " " >> $C_LOG_PATH
 
 export C_PROJECT_URL=$KC_NWTTLS__BASE_URL  # include trailing slash
 
